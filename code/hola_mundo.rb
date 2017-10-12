03e3bcb8016672afbf42cd4270d533cbb72aef7e
chr1spolo <polidavis28@gmail.com>
2017-10-12 17:36:27 -0500

    Usando el condicional if

---
+++ code/hola_mundo.rb
@@ -0,0 +1,11 @@
+puts "¿Cual es tu nombre?"
+
+nombre = gets.chomp
+
+if nombre == 'Christian'
+    puts "Bienvenido #{nombre}, eres el máster."
+elseif == 'Linda'
+    puts "Eres la mujer más hermosa del planeta."
+else
+    puts "Hola, bienvenido."
+end
