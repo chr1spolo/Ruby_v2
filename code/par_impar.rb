b3491747cba4305f3d3b8a65f3e412dddb4b6b65
chr1spolo <polidavis28@gmail.com>
2017-10-12 22:00:11 -0500

    añadiendo metodos y determinando si un año es biciesto.

--- code/par_impar.rb
+++ code/par_impar.rb
@@ -1,8 +1,14 @@
-puts "ingrese un numero: " 
-numero = (gets.chomp.to_i) % 2
-
-if numero == 0
-    puts "es par"
-else
-    puts "es impar"
+#ruby siempre tiene un valor de retorno que por defecto es lo que devuelve la ultima linea
+#con el signo de interrogación denotamos que  el metodo devolverá un booleano
+def es_par?(num)
+    if num % 2 == 0
+        return "es par"
+    else
+        return "es impar"
+    end
 end
+
+puts "ingrese un numero: " 
+numero = gets.chomp.to_i
+
+puts es_par?(numero)
