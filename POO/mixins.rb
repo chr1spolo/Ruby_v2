8ac074a750056fb53e334e9f6f281236f7f3c3c3
chr1spolo <polidavis28@gmail.com>
2017-10-19 17:48:36 -0500

    mixins

---
+++ POO/mixins.rb
@@ -0,0 +1,41 @@
+
+#utilizando include
+module Conversion
+    def palabra
+        return 'uno' if @valor == 1
+        'mucho'
+    end
+end
+
+class Numero
+    include Conversion
+    attr_accessor :valor
+end
+
+num = Numero.new
+num.valor = 1
+
+puts num.palabra
+
+
+#utilizando extend
+module Conversion
+    def palabra
+        return 'uno' if @valor == 1
+        'mucho'
+    end
+end
+
+class Numero
+    attr_accessor :valor
+    def palabra
+        @valor.to_s
+    end
+end
+
+num = Numero.new
+num.valor = 1
+puts num.palabra
+
+num.extend(Conversion)
+puts num.palabra
